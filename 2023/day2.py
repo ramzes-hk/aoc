28445b15cbc647c87a71ad68a8152f84a5cff4d6
Ramzes <68265608+ramzes-hk@users.noreply.github.com>
2023-12-02 10:45:35 -0500

    day2 part2

--- 2023/day2.py
+++ 2023/day2.py
@@ -16,6 +16,23 @@ def part1(lines: list[str]):
     print(total)
 
 
+def part2(lines: list[str]):
+    total = 0
+    for line in lines:
+        total_round = 0
+        max = {"red": 0, "green": 0, "blue": 0}
+        handfuls = line.strip().split(":")[1].split(";")
+        for handful in handfuls:
+            cubes = handful.strip().split(",")
+            cubes = [cube.strip().split(" ") for cube in cubes]
+            for cube in cubes:
+                if max[cube[1]] < int(cube[0]):
+                    max[cube[1]] = int(cube[0])
+            total_round = max["red"] * max["green"] * max["blue"]
+        total += total_round
+    print(total)
+
+
 with open("day2.txt", "r") as f:
     lines = f.readlines()
-part1(lines)
+part2(lines)
