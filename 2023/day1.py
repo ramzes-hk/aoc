f8d74fab0d7f77245efa612550269e4bff65e987
Ramzes <68265608+ramzes-hk@users.noreply.github.com>
2023-12-01 21:44:52 -0500

    formatted day1

--- 2023/day1.py
+++ 2023/day1.py
@@ -1,21 +1,22 @@
-def part1(lines: list[str]): 
+def part1(lines: list[str]):
     total = 0
     for line in lines:
-        first: str = '0'
-        last: str = '0'
+        first: str = "0"
+        last: str = "0"
         for char in line:
-            if char.isdigit() and first == '0':
+            if char.isdigit() and first == "0":
                 first = char
-                last = char 
+                last = char
             elif char.isdigit():
                 last = char
         total += int(first + last)
     print("\ntotal >>>>>", total)
 
+
 def part2(lines: list[str]):
     numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     for i in range(len(lines)):
-        string: str = "" 
+        string: str = ""
         for char in lines[i]:
             string += char
             for n in range(9):
@@ -23,6 +24,7 @@ def part2(lines: list[str]):
         lines[i] = string
         print(lines[i])
     part1(lines)
+
 
 with open("day1.txt", "r") as f:
     lines = f.readlines()
