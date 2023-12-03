5db0935132022b697792432c1b42e2c484b30d5d
Ramzes <68265608+ramzes-hk@users.noreply.github.com>
2023-12-03 13:07:21 -0500

    day3 part2 (another day another warcrime)

--- 2023/day3.py
+++ 2023/day3.py
@@ -8,16 +8,13 @@ class Number:
         self.xb = xb
         self.xe = xe
         self.y = y
+        self.checked = False 
 
     def __repr__(self) -> str:
         return f"{self.value} {self.xb} {self.xe} {self.y}\n"
 
-
-def part1(lines: list[str]):
-    width = len(lines[0]) - 1
-    height = len(lines) - 1
+def numberify(lines: list[str]) -> list[Number]:
     numbers: list[Number] = []
-
     for i, line in enumerate(lines):
         current_number = ""
         xb = 0
@@ -32,17 +29,24 @@ def part1(lines: list[str]):
                 current_number = ""
             else:
                 continue
+    return numbers 
+
+def clamp(n: int, m: int) -> int:
+    return max(0, min(m, n))
+
+def part1(lines: list[str]):
+    width = len(lines[0]) - 1
+    height = len(lines) - 1
+    numbers = numberify(lines)
+
     total = 0
     for number in numbers:
         flag = False
         for x in range(number.xb, number.xe + 1):
             for [ver, hor] in directions:
-                if (
-                    lines[max(0, min(height, number.y + ver))][
-                        max(0, min(width, x + hor))
-                    ]
-                    in symbols
-                ):
+                sx = clamp(x + ver, width)
+                sy = clamp(number.y + hor, height)
+                if lines[sy][sx] in symbols:
                     total += number.value
                     flag = True
                     break
@@ -50,6 +54,35 @@ def part1(lines: list[str]):
                 break
     print(total)
 
+def part2(lines: list[str]):
+    numbers = numberify(lines)
+    height = len(lines) - 1
+    width = len(lines[0]) - 1
+    total = 0
+
+    for number in numbers:
+        if number.checked:
+            continue
+        number.checked = True 
+        flag = False
+        for x in range(number.xb, number.xe + 1):
+            for [ver, hor] in directions:
+                sy = clamp(number.y + ver, height)
+                sx = clamp(x + hor, width)
+                if lines[sy][sx] == '*':
+                    for [ver, hor] in directions:
+                        star_y = clamp(sy + ver, height)
+                        star_x = clamp(sx + hor, width)
+                        for star_number in numbers: 
+                            if star_number.checked:
+                                continue
+                            if star_number.y == star_y and star_x in range(star_number.xb, star_number.xe + 1) and not star_number.checked:
+                                total += number.value * star_number.value
+                                star_number.checked = True
+                    break
+            if flag:
+                break
+    print(total)
 
 with open("day3.txt", "r") as f:
     lines = f.readlines()
@@ -60,4 +93,4 @@ with open("day3.txt", "r") as f:
     #             continue
     #         symbols.update(char)
     # print(symbols)
-    part1(lines)
+    part2(lines)
