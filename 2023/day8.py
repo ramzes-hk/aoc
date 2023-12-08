bbeec113348a45cd6ef8f6b74f30344f61166cfa
Ramzes <68265608+ramzes-hk@users.noreply.github.com>
2023-12-08 15:03:47 -0500

    day8 part2

--- 2023/day8.py
+++ 2023/day8.py
@@ -1,4 +1,19 @@
 from io import TextIOWrapper
+from math import gcd
+
+
+def lcm(a, b):
+    """Calculate the Least Common Multiple of two numbers."""
+    return a * b // gcd(a, b)
+
+
+def lcd(array):
+    """Find the Lowest Common Denominator (LCD) for an array of numbers."""
+    # Initialize the result with the first number in the array
+    result = array[0]
+    for i in range(1, len(array)):
+        result = lcm(result, array[i])
+    return result
 
 
 def part1(f: TextIOWrapper):
@@ -25,5 +40,31 @@ def part1(f: TextIOWrapper):
     print(count)
 
 
+def part2(f: TextIOWrapper):
+    line = f.readline()
+    moves: str = ""
+    destinations: dict[str, list[str]] = dict()
+    while line:
+        match line.split():
+            case [dir]:
+                moves = dir
+            case [dest, "=", left, right]:
+                destinations.update({dest: [left[1:-1], right[:-1]]})
+        line = f.readline()
+    current = [key for key in destinations.keys() if key[2] == "A"]
+    node_counters = [0 for _ in range(len(current))]
+
+    for i in range(len(current)):
+        while current[i][2] != "Z":
+            for move in moves:
+                next = destinations[current[i]]
+                if move == "L":
+                    current[i] = next[0]
+                else:
+                    current[i] = next[1]
+                node_counters[i] += 1
+    print(lcd(node_counters))
+
+
 with open("day8.txt", "r") as f:
-    part1(f)
+    part2(f)
